c3395954d63a55cee3a24a70c79821e2f5cc9649
MaxTitovitch <maxtitovitch@mail.ru>
2020-09-01 18:10:08 +0300

    Restore sms sending created

--- services/Sender.js
+++ services/Sender.js
@@ -1,11 +1,12 @@
 let FCM = require('fcm-node');
 let serverKey = 'AAAA_zkXm70:APA91bEhCvRxVXWS7pfayvUALYZHL5JQ5Ir6y_REzeKmJaSdcUIv9sYbCA5Ajh2es07jmzFTDP2bOSEAmfM05jSRajoD_nICGpajbMN05M7NsHTey2ITqdobDRd60c9B8iO9Wgjn2Kt_';
 let fcm = new FCM(serverKey);
+const queryString = require('query-string');
 
 const axios = require('axios').default;
 const userName = 'jsay';
 const password = 'jsay2020';
-
+axios.defaults.headers.get['Content-Type'] = 'application/json, text/plain, */*; charset=utf-8';
 
 
 module.exports = class Service {
@@ -30,15 +31,13 @@ module.exports = class Service {
     }
 
     static sendSMS(phone, code) {
-        let data = {
+        let query = queryString.stringify({
             user: userName,
             pass: password,
             to: phone.substr(1),
-            txt: `Code: ${code}`
-        };
-        let query =  Object.keys(data).map(key => `${key}=${data[key]}`).join('&');;
-        let path = `https://api3.greensms.ru/sms/send?${query}`;
-        axios.get(path)
+            txt: code
+        });
+        axios.get(`https://api3.greensms.ru/sms/send?${query}`)
             .then(function (response) {
                 console.log(response);
             })
